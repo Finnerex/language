4f18b3b1b40b57d1078106f5a793f50e1689eca7
gtant <gavin.f.tantleff@gmail.com>
2023-11-14 02:10:48 +0000

    state tuple

--- bin/interpreter.ml
+++ bin/interpreter.ml
@@ -8,84 +8,110 @@ exception Unimplemented
 
 let rec eval_expr (state:PrgmSt.t) (e:expr) =
   match e with
-  | Int x -> Int x
-  | Bool x -> Bool x
-  | EString x -> EString x
-  | Systime -> Int (int_of_float (Sys.time () *. 1000.0))
+  | Int x -> (Int x, state)
+  | Bool x -> (Bool x, state)
+  | EString x -> (EString x, state)
+  | Systime -> (Int (int_of_float (Sys.time () *. 1000.0)), state)
 
   | Plus(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Int(i1 + i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Int(i1 + i2), state3)
     | _ -> raise TypeMismatch)
   
   | Minus(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Int(i1 - i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Int(i1 - i2), state3)
     | _ -> raise TypeMismatch)
     
   | Times(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Int(i1 * i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Int(i1 * i2), state3)
     | _ -> raise TypeMismatch)
     
   | Div(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Int(i1 / i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Int(i1 / i2), state3)
     | _ -> raise TypeMismatch)
 
   | Modulo(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Int(i1 mod i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Int(i1 mod i2), state3)
     | _ -> raise TypeMismatch)
   
   | Less(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Bool(i1 < i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Bool(i1 < i2), state3)
     | _ -> raise TypeMismatch)
   
   | LessEq(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Bool(i1 <= i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Bool(i1 <= i2), state3)
     | _ -> raise TypeMismatch)
   
   | Greater(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Bool(i1 > i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Bool(i1 > i2), state3)
     | _ -> raise TypeMismatch)
   
   | GreaterEq(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Bool(i1 >= i2)
+    let i1, state2 = eval_expr state e1 in
+    let i2, state3 = eval_expr state2 e2 in
+    (match i1, i2 with
+    | Int i1, Int i2 -> (Bool(i1 >= i2), state3)
     | _ -> raise TypeMismatch)
 
   | Not(x) ->
-    (match eval_expr state x with
-    | Bool b -> Bool (not b)
+    let new_x, new_state = eval_expr state x in
+    (match new_x with
+    | Bool b -> (Bool (not b), new_state)
     | _ -> raise TypeMismatch)
 
   | And(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Bool b1, Bool b2 -> Bool(b1 && b2)
+    let b1, state2 = eval_expr state e1 in
+    let b2, state3 = eval_expr state2 e2 in
+    (match b1, b2 with
+    | Bool b1, Bool b2 -> (Bool(b1 && b2), state3)
     | _ -> raise TypeMismatch)
 
   | Or(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Bool b1, Bool b2 -> Bool(b1 || b2)
+    let b1, state2 = eval_expr state e1 in
+    let b2, state3 = eval_expr state2 e2 in
+    (match b1, b2 with
+    | Bool b1, Bool b2 -> (Bool(b1 || b2), state3)
     | _ -> raise TypeMismatch)
 
   | Equals(e1, e2) ->
-    (match eval_expr state e1, eval_expr state e2 with
-    | Int i1, Int i2 -> Bool(i1 = i2)
-    | Bool b1, Bool b2 -> Bool(b1 = b2)
+    let x1, state2 = eval_expr state e1 in
+    let x2, state3 = eval_expr state2 e2 in
+    (match x1, x2 with
+    | Int i1, Int i2 -> (Bool(i1 = i2), state3)
+    | Bool b1, Bool b2 -> (Bool(b1 = b2), state3)
     | _ -> raise TypeMismatch)
 
   | Ternary(c, e1, e2) ->
-    (match eval_expr state c with
-    | Bool true -> eval_expr state e1
-    | Bool false -> eval_expr state e2
+    let new_c, new_state = eval_expr state c in
+    (match new_c with
+    | Bool true -> eval_expr new_state e1
+    | Bool false -> eval_expr new_state e2
     | _ -> raise TypeMismatch)
 
-  | Var(i) -> PrgmSt.find_var state i
+  | Var(i) -> (PrgmSt.find_var state i, state)
   
   (* | _ -> raise Unimplemented *)
 
@@ -94,11 +120,18 @@ let rec flatten_list (accum:'b -> 'a -> 'b) (l:'a list) (i:'b) =
   | [] -> i
   | sm :: l2 -> accum i sm |> flatten_list accum l2
 
+let rec map_list_expr el s =
+  match el with
+  | [] -> []
+  | e::new_el -> let ce, new_s = eval_expr s e in
+    ce::map_list_expr new_el new_s
+
 let rec eval_statement (state:PrgmSt.t) (sm:statement) = 
   match sm with
 
   | Assign(v, e) -> 
-    PrgmSt.add_var state v (eval_expr state e)
+    let new_e, new_state = eval_expr state e in
+    PrgmSt.add_var new_state v new_e
   
   | FuncDef(i, vl, sml) ->
     PrgmSt.add_func state i (vl, sml)
@@ -106,25 +139,27 @@ let rec eval_statement (state:PrgmSt.t) (sm:statement) =
   | FuncCall(i, el) ->
     let (vl, sml) = PrgmSt.find_func state i in
     let pushed_state = PrgmSt.push_stack state in
-    let new_state = List.combine vl (List.map (eval_expr pushed_state) el) |> PrgmSt.add_vars pushed_state in
+    let new_state = List.combine vl (map_list_expr el pushed_state) |> PrgmSt.add_vars pushed_state in
     flatten_list eval_statement sml new_state |> PrgmSt.pop_stack
 
   | If(l) ->
     (match l with
     | [] -> state
     | (b, sml) :: xs ->
-      (match eval_expr state b with
-      | Bool true -> flatten_list eval_statement sml state
-      | Bool false -> eval_statement state (If xs)
+      let new_b, new_state = eval_expr state b in
+      (match new_b with
+      | Bool true -> flatten_list eval_statement sml new_state
+      | Bool false -> eval_statement new_state (If xs)
       | _ -> raise TypeMismatch))
   
   | While(e, sml) ->
-    (match eval_expr state e with
+    let new_e, new_state = eval_expr state e in
+    (match new_e with
     | Bool true -> 
-      let new_state = flatten_list eval_statement sml state in 
-      eval_statement new_state sm
+      let new_new_state = flatten_list eval_statement sml new_state in 
+      eval_statement new_new_state sm
 
-    | Bool false -> state
+    | Bool false -> new_state
     | _ -> raise TypeMismatch)
 
   | For(is, e, ls, sml) ->
@@ -132,12 +167,13 @@ let rec eval_statement (state:PrgmSt.t) (sm:statement) =
     eval_statement new_state (While(e, sml @ [ls]))
 
   | Print(e) -> 
-    Printf.printf "%s" (match eval_expr state e with
+    let new_e, new_state = eval_expr state e in
+    Printf.printf "%s" (match new_e with
      | Int x -> string_of_int x
      | Bool x -> string_of_bool x
      | EString x -> x
      | _ -> "somethin else");
-     state
+     new_state
 
   | PrintLn(e) ->
     let state = eval_statement state (Print e) in
